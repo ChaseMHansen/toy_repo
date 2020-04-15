de8b7464820546f458507791a277ba9d9a7de2cd
Chase Hansen <cmhansen@ucdavis.edu>
2020-04-15 08:25:19 -0700

    Updated based on Code Review. Intentionally left out a script, as I want this to be something that can be imported, so I don't think I want a script contained here.

--- 3_state_CA.py
+++ 3_state_CA.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def random_string(length):
-    '''
+    """
     Returns a random bit string of the given length. 
     
     Parameters
@@ -16,13 +16,14 @@ def random_string(length):
     -------
     out: list
         The random bit string given as a list, with int elements.
-    '''
+    """
     if not isinstance(length, int) or length < 0:
         raise ValueError("input length must be a positive ingeter")
-    return [random.randint(0,1) for _ in range(length)]
+    return [random.randint(0, 1) for _ in range(length)]
+
 
 def random_Tern_string(length):
-    '''
+    """
     Returns a random bit string of the given length. 
     
     Parameters
@@ -34,17 +35,19 @@ def random_Tern_string(length):
     -------
     out: list
         The random bit string given as a list, with int elements.
-    '''
+    """
     if not isinstance(length, int) or length < 0:
         raise ValueError("input length must be a positive ingeter")
-    return [random.randint(0,2) for _ in range(length)]
+    return [random.randint(0, 2) for _ in range(length)]
+
 
 class ECA(object):
-    '''
+    """
     Elementary cellular automata simulator.
-    '''
+    """
+
     def __init__(self, rule_number, initial_condition, num_neighborhoods, num_states):
-        '''
+        """
         Initializes the simulator for the given rule number and initial condition.
 
         Parameters
@@ -55,7 +58,7 @@ class ECA(object):
         initial_condition: list
             Ternary string used as the initial condition for the ECA. Elements of the list
             should be ints.
-        num_Neighborhoods: int
+        num_neighborhoods: int
             The number of possible neighborhoods, for binary, we had 8, for ternary, 9.
         num_states: int
             The number of states for each cell in our CA, 2 or 3.
@@ -70,15 +73,14 @@ class ECA(object):
             2D array (list of lists) of the spacetime field created by the simulator.
         current_configuration: array_like
             List of the spatial configuration of the ECA at the current time
-        '''
+        """
         # we will see a cleaner and more efficient way to do the following when we introduce numpy
         for i in initial_condition:
-            if i not in [0,1,2]:
+            if i not in [0, 1, 2]:
                 raise ValueError("initial condition must be a list of 0s and 1s")
 
-
         self.num_states = num_states
-        self.ruleNumber=rule_number
+        self.rule_number = rule_number
         self.num_neighborhoods = num_neighborhoods
         self.lookup_table = self.generate_lookup_table()
         self.initial = initial_condition
@@ -86,116 +88,135 @@ class ECA(object):
         self.current_configuration = initial_condition.copy()
         self._length = len(initial_condition)
 
-
-
     def change_base(self):
-        '''
+        """
         Change a number into base 2 or base 3, with 8 or 9 digits respectively.
-        '''
-        if self.num_states==2:
-            rule_remaining=self.ruleNumber
-            intList=[]
+        """
+        if self.num_states == 2:
+            rule_remaining = self.rule_number
+            intList = []
             for i in range(self.num_neighborhoods):
-                if rule_remaining/2**(self.num_neighborhoods-i-1)>=1:
+                if rule_remaining / 2 ** (self.num_neighborhoods - i - 1) >= 1:
                     intList.append(1)
-                    rule_remaining += -2**(self.num_neighborhoods-i-1)
+                    rule_remaining += -(2 ** (self.num_neighborhoods - i - 1))
                     continue
-                if rule_remaining/2**(self.num_neighborhoods-i-1)<1:
+                if rule_remaining / 2 ** (self.num_neighborhoods - i - 1) < 1:
                     intList.append(0)
                     continue
             return intList
 
-        if self.num_states==3:
-            #We have 9 neighborhoods, so we need 9 digits of ternary.
-            rule_remaining=self.ruleNumber
-            intList=[]
+        if self.num_states == 3:
+            # We have 9 neighborhoods, so we need 9 digits of ternary.
+            rule_remaining = self.rule_number
+            intList = []
             for i in range(self.num_neighborhoods):
-                if rule_remaining/3**(self.num_neighborhoods-i-1)>=2:
+                if rule_remaining / 3 ** (self.num_neighborhoods - i - 1) >= 2:
                     intList.append(2)
-                    rule_remaining += -3**(self.num_neighborhoods-i-1)*2
+                    rule_remaining += -(3 ** (self.num_neighborhoods - i - 1)) * 2
                     continue
-                if rule_remaining/3**(self.num_neighborhoods-i-1)>=1 and rule_remaining/3**(self.num_neighborhoods-i-1)<2:
+                if (
+                    rule_remaining / 3 ** (self.num_neighborhoods - i - 1) >= 1
+                    and rule_remaining / 3 ** (self.num_neighborhoods - i - 1) < 2
+                ):
                     intList.append(1)
-                    rule_remaining += -3**(self.num_neighborhoods-i-1)*1
+                    rule_remaining += -(3 ** (self.num_neighborhoods - i - 1)) * 1
                     continue
-                if rule_remaining/3**(self.num_neighborhoods-i-1)<1:
+                if rule_remaining / 3 ** (self.num_neighborhoods - i - 1) < 1:
                     intList.append(0)
                     continue
 
             return intList
 
     def generate_lookup_table(self):
-        '''
+        """
         Take the rule number and the number of states to make a mapping from a
         string in base 2 or base 3 into a propagator that takes
         the system to the next timestep.
-        '''
-        Digits=self.change_base()
-        neighborhoods=self.create_Neighborhoods()
+        """
+        digits = self.change_base()
+        neighborhoods = self.create_neighborhoods()
 
+        mapping = dict()
 
-        mapping=dict()
-
-        for i in range(len(Digits)):
-            #Here I want to flip the binary or ternary string string, such that 
-            #the 0,0,0 or 0,0 neighborhood lines up with the final digit.
-            mapping.update({neighborhoods[i] : Digits[len(Digits)-i-1]})
+        for i in range(len(digits)):
+            # Here I want to flip the binary or ternary string string, such that
+            # the 0,0,0 or 0,0 neighborhood lines up with the final digit.
+            mapping.update({neighborhoods[i]: digits[len(digits) - i - 1]})
         return mapping
-    def create_Neighborhoods(self):
-        '''
+
+    def create_neighborhoods(self):
+        """
         Create the allowed neighborhoods for either 2 or 3 states.
-        '''
-        if self.num_states==2:
-            nbhds=[(0,0,0), (0,0,1), (0,1,0), (0,1,1), (1,0,0), (1,0,1), 
-                    (1,1,0), (1,1,1)]
-        if self.num_states==3:
-            nbhds=[(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1),
-                    (2,2)]
+        """
+        if self.num_states == 2:
+            nbhds = [
+                (0, 0, 0),
+                (0, 0, 1),
+                (0, 1, 0),
+                (0, 1, 1),
+                (1, 0, 0),
+                (1, 0, 1),
+                (1, 1, 0),
+                (1, 1, 1),
+            ]
+        if self.num_states == 3:
+            nbhds = [
+                (0, 0),
+                (0, 1),
+                (0, 2),
+                (1, 0),
+                (1, 1),
+                (1, 2),
+                (2, 0),
+                (2, 1),
+                (2, 2),
+            ]
         return nbhds
 
-
     def evolve_CA(self, num_steps):
-        '''
+        """
         Take the initial condition and evolve it num_steps timesteps using
         the propagator defined by the
         rule number.
-        '''
+        """
         initial_condition = self.initial
         current_configuration = initial_condition.copy()
-        propagator=self.generate_lookup_table()
+        propagator = self.generate_lookup_table()
 
-        Config_Placeholder=initial_condition.copy()
-        time_series=[]
+        Config_Placeholder = initial_condition.copy()
+        time_series = []
         time_series.append(current_configuration)
-        if self.num_states==2:
-            for i in range(num_steps):
+        if self.num_states == 2:
+            for _ in range(num_steps):
                 for x in range(len(current_configuration)):
-                    xnbhd=(int(current_configuration[x-1]),
-                            int(current_configuration[x]),
-                            int(current_configuration[(x+1)%self._length]))
+                    xnbhd = (
+                        int(current_configuration[x - 1]),
+                        int(current_configuration[x]),
+                        int(current_configuration[(x + 1) % self._length]),
+                    )
 
-                    Config_Placeholder[x]=int(propagator[xnbhd])
-                current_configuration=Config_Placeholder.copy()
+                    Config_Placeholder[x] = int(propagator[xnbhd])
+                current_configuration = Config_Placeholder.copy()
                 time_series.append(current_configuration)
         if self.num_states == 3:
-            for i in range(num_steps):
+            for _ in range(num_steps):
                 for x in range(len(current_configuration)):
-                    xnbhd=(int(current_configuration[x-1]),
-                            int(current_configuration[x]))
+                    xnbhd = (
+                        int(current_configuration[x - 1]),
+                        int(current_configuration[x]),
+                    )
 
-                    Config_Placeholder[x]=int(propagator[xnbhd])
-                current_configuration=Config_Placeholder.copy()
+                    Config_Placeholder[x] = int(propagator[xnbhd])
+                current_configuration = Config_Placeholder.copy()
                 time_series.append(current_configuration)
         return time_series
 
-    def plot_CA(self,num_steps):
-        '''
+    def plot_CA(self, num_steps):
+        """
         Plot the result of a CA calculation.
-        '''
-        time_series=self.evolve_CA(num_steps)
-        plt.figure(figsize=(12,12))
-        #My girlfriend wanted pink, so the colormap has pink.
-        plt.imshow(time_series, cmap=plt.cm.spring, interpolation='nearest')
+        """
+        time_series = self.evolve_CA(num_steps)
+        plt.figure(figsize=(12, 12))
+        # My girlfriend wanted pink, so the colormap has pink.
+        plt.imshow(time_series, cmap=plt.cm.spring, interpolation="nearest")
         plt.show()
-
-
